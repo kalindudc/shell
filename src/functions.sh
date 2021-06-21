412f77ddfd2642dd006d64e35b52aadf5f0394eb
Kalindu De Costa <kalindu.decosta@shopify.com>
2021-06-21 12:51:51 -0400

    Add new functions for git checkout

--- src/functions.sh
+++ src/functions.sh
@@ -6,3 +6,21 @@ reload() {
   [[ $HOME/.zshrc ]] && source $HOME/.zshrc
   # [[ $HOME/.bashrc ]] && source $HOME/.bashrc
 }
+
+newbranch() {
+  if [[ $# -gt 0 ]]; then
+    git checkout -b $1
+    export BRANCH=$1
+  else
+    echo "Invalid use of \`newbranch\`. Please provide a name for the new branch."
+  fi
+}
+
+checkout() {
+  if [[ $# -gt 0 ]]; then
+    git checkout $1
+    export BRANCH=$1
+  else
+    echo "Invalid use of \`checkout\`. Please provide a name for the new branch."
+  fi
+}
