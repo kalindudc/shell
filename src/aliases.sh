7c116b2b718876d9b81eb36c4415a617ac2be5fa
Kalindu De Costa <kalindu.decosta@shopify.com>
2021-12-14 10:17:53 -0500

    Improve date calculation for slacks binary

--- src/aliases.sh
+++ src/aliases.sh
@@ -7,6 +7,12 @@ CURRENT_DIR="$(
   pwd -P
 )"
 
+MY_BASH_BLUE="\033[0;34m" #Blue
+MY_BASH_NOCOLOR="\033[0m"
+HISTTIMEFORMAT=`echo -e ${MY_BASH_BLUE}[%F %T] $MY_BASH_NOCOLOR `
+HISTSIZE=20000
+HISTFILESIZE=20000
+
 alias grepn='grep -n'
 
 # terminal multiplexers
@@ -19,7 +25,15 @@ alias lsg='lsa --color=yes | grep -i $@ '
 alias cpv='rsync -ah --info=progress2'
 
 alias mnt='mount | grep -E ^/dev | column -t'
-alias history='history -i |grep'
+alias hs='custom-history'
 alias cl='clear'
 alias psg='ps -ef | grep'
 alias myip='curl ip.me'
+
+custom-history() {
+  if [[ $# -lt 1 ]]; then
+    history -i
+  else
+    history -i | grep $@
+  fi
+}
